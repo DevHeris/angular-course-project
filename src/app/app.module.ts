284e51308bdb1245c65696ea4e2d3e3f4caf7916
DevHeris <badairoinioluwa578@gmail.com>
2024-09-25 16:05:28 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './features/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './features/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './features/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './features/recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
