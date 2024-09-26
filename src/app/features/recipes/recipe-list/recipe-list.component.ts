804f05a09a81f8a57925fc06e30199300093fb13
DevHeris <badairoinioluwa578@gmail.com>
2024-09-26 11:31:04 +0100

    upadate

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?t=st=1727346062~exp=1727349662~hmac=4eef42dc9a5ff819a6d7858d2d916b20efd9af7d03a64fae65a45fdf66d581b2&w=740',
+      'This is simply a test'
+    ),
+  ];
 }
---
+++ src/app/features/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+// Recipe model
+export class Recipe {
+  public name: string;
+  public imagePath: string;
+  public description: string;
+
+  constructor(name: string, imagePath: string, description: string) {
+    this.name = name;
+    this.imagePath = imagePath;
+    this.description = description;
+  }
+}
