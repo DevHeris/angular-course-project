9514400b8d7c747a28237c01539e0f9a0eda27ef
DevHeris <badairoinioluwa578@gmail.com>
2024-10-08 12:17:32 +0100

    update

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,14 +14,14 @@ export class RecipeListComponent {
       'This is simply a test'
     ),
     new Recipe(
-      'A test Recipe',
+      'Another test Recipe',
       'https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?t=st=1727346062~exp=1727349662~hmac=4eef42dc9a5ff819a6d7858d2d916b20efd9af7d03a64fae65a45fdf66d581b2&w=740',
-      'This is simply a test'
-    ),
-    new Recipe(
-      'A test Recipe',
-      'https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?t=st=1727346062~exp=1727349662~hmac=4eef42dc9a5ff819a6d7858d2d916b20efd9af7d03a64fae65a45fdf66d581b2&w=740',
-      'This is simply a test'
+      'This is a different test'
     ),
   ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  onSelect(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
--- src/app/features/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/features/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrl: './shopping-list-edit.component.css'
+  styleUrl: './shopping-list-edit.component.css',
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
+  @Output() newIngredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+
+    if (ingName === '' || ingAmount === '') return;
+
+    this.newIngredientAdded.emit(new Ingredient(ingName, Number(ingAmount)));
+
+    this.nameInputRef.nativeElement.value = '';
+    this.amountInputRef.nativeElement.value = '';
+  }
 }
